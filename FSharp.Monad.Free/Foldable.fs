b63cc10d3ef96c416739c67938350c3031625364
pocketberserker <pocketberserker@gmail.com>
2014-09-18 00:06:01 +0900

    add Foldable

---
+++ FSharp.Monad.Free/Foldable.fs
@@ -0,0 +1,10 @@
+﻿namespace FSharp.Monad
+
+type Foldable<'F> =
+  abstract member FoldMap: F1<'A, 'B> * _1<'F, 'A> * #Monoid<'B> -> 'B
+  abstract member Fold: _1<'F, 'A> * 'B * F2<'B, 'A, 'B> -> 'B
+
+module Foldable =
+
+  let foldMap (f: #Foldable<_>) M (F1 g) fa = f.FoldMap(g, fa, M)
+  let fold (f: #Foldable<_>) (F2 g) b fa = f.Fold(fa, b, g)
