991c1b025a53e731707edefac8cb70d0e97e3755
pocketberserker <pocketberserker@gmail.com>
2014-09-17 23:50:22 +0900

    add Monoid

---
+++ FSharp.Monad.Free/Monoid.fs
@@ -0,0 +1,10 @@
+﻿namespace FSharp.Monad
+
+type Monoid<'F> =
+  inherit Semigroup<'F>
+  abstract member Zero: unit -> 'F
+
+module Monoid =
+
+  let zero (m: #Monoid<_>) = m.Zero()
+  let append (m: #Monoid<_>) a b = m.Append(a, b)
