f5102c46a9505e1dd7b434016665219d3c343f62
pocketberserker <pocketberserker@gmail.com>
2014-09-18 08:11:09 +0900

    add instance function

--- FSharp.Monad.Free/Monoid.fs
+++ FSharp.Monad.Free/Monoid.fs
@@ -8,3 +8,7 @@ module Monoid =
 
   let zero (m: #Monoid<_>) = m.Zero()
   let append (m: #Monoid<_>) a b = m.Append(a, b)
+
+  let instance f z = { new Monoid<_> with
+    member this.Zero() = z
+    member this.Append(f1, f2) = f f1 f2 }
--- FSharp.Monad.Free/Semigroup.fs
+++ FSharp.Monad.Free/Semigroup.fs
@@ -6,3 +6,6 @@ type Semigroup<'F> =
 module Semigroup =
 
   let append (s: #Semigroup<_>) a b = s.Append(a, b)
+
+  let instance f = { new Semigroup<_> with
+    member this.Append(f1, f2) = f f1 f2 }
