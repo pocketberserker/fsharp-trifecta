2450a06347224fddbedf262e3d37e64837f16ffc
y.nakayama <y.nakayama@otr-jp.com>
2014-09-18 09:11:04 +0900

    add option helper functions

---
+++ FSharp.Monad.Free/Option.fs
@@ -0,0 +1,20 @@
+﻿namespace FSharp.Monad
+
+module Option =
+
+  let monoid (s: Semigroup<_>) = { new Monoid<_> with
+    member this.Append(f1, f2) =
+      match (f1, f2) with
+      | Some a1, Some a2 -> Some (s.Append(a1, a2))
+      | Some _, None -> f1
+      | None, Some _ -> f2
+      | None, None -> None
+    member this.Zero() = None }
+
+  let getOrElse defaultValue = function
+    | Some v -> v
+    | None -> defaultValue
+
+  let orElse d = function
+    | Some _ as v -> v
+    | None -> d ()
